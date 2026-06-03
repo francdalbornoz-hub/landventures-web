6dd50fb4ca2915a43ab7c8f0de8e9d94cd588b84
francdalbornoz-hub <francdalbornoz@gmail.com>
2026-06-03 10:51:41 -0300

    chore(proyectos): remove unused Link import

--- app/proyectos/page.tsx
+++ app/proyectos/page.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import Link from 'next/link';
 import { projects } from '@/lib/content/projects';
 import { buildMetadata, breadcrumbJsonLd } from '@/lib/seo';
 import JsonLd from '@/components/JsonLd';
